0b9b43e8d39cee8acde739c82e1b04b4efa99ff4
Codebulit <109361363+Codebulit@users.noreply.github.com>
2022-07-18 23:33:15 +0530

    Add files via upload

---
+++ Assign4.cpp
@@ -0,0 +1,131 @@
+
+/*UNCOMMEMT THE PARTICULAR CODE BELOW THE QUESTION TO GET THE SOLUTION*/
+
+// 1. Write a program to print MySirG 5 times on the screen
+  
+//   #include<stdio.h>
+//   int main() {
+//   for(int i=1;i<=5;i++){
+//     printf("MySirG\n");
+//   }
+//   return 0;
+//   }
+
+//=====================================================================
+
+// 2. Write a program to print the first 10 natural numbers.
+
+// #include<stdio.h>
+// int main() {
+// int i;
+// printf("The first 10 Natural number is:\n");
+// for(i=1;i<=10;i++){
+    
+//     printf("%d ",i);
+// }
+// return 0;
+// }
+//===================================================================
+
+// 3. Write a program to print the first 10 natural numbers in reverse order
+
+// #include<stdio.h>
+// int main() {
+// int i;
+// printf("first 10 natural numbers in reverse order \n");
+// for(i=10;i>=1;i--){
+//     printf("%d ",i);
+// }
+// return 0;
+// }
+//=======================================================================
+
+// 4. Write a program to print the first 10 odd natural numbers
+
+// #include<stdio.h>
+// int main() {
+// int i;
+// printf("the first 10 odd natural numbers \n");
+// for(i=1;i<=20;i+=2){
+//     printf("%d ",i);
+
+// }
+// return 0;
+//}
+//=========================================================================
+
+// 5. Write a program to print the first 10 odd natural numbers in reverse order.
+
+// #include<stdio.h>
+// int main() {
+// int i;
+// printf("the first 10 odd natural numbers in reverse order:\n");
+// for(i=20;i>=1;i--){
+//     printf("%d ",i);
+// }
+// return 0;
+// }
+//===========================================================================
+
+// 6. Write a program to print the first 10 even natural numbers
+
+// #include<stdio.h>
+// int main() {
+// int i;
+// printf("the first 10 even natural numbers: \n");
+// for(i=2;i<=20;i+=2){
+//     printf("%d ",i);
+// }
+// return 0;
+// }
+//==============================================================================
+
+// 7. Write a program to print the first 10 even natural numbers in reverse order
+
+// #include<stdio.h>
+// int main() {
+// int i;
+// printf(" the first 10 even natural numbers in reverse order:\n");
+// for(i=20;i>=1;i-=2){
+//     printf("%d ",i);
+// }
+// return 0;
+// }
+//===============================================================================
+
+// 8. Write a program to print squares of the first 10 natural numbers
+
+// #include<stdio.h>
+// int main() {
+// int i;
+// printf("squares of the first 10 natural numbers: \n");
+// for(i=1;i<=10;i++){
+//      printf("%d ",i*i);
+// }
+// return 0;
+// }
+//=============================================================================
+
+// 9. Write a program to print cubes of the first 10 natural numbers
+
+// #include<stdio.h>
+// int main() {
+// int i;
+// printf("squares of the first 10 natural numbers: \n");
+// for(i=1;i<=10;i++){
+//      printf("%d ",i*i*i);
+// }
+// return 0;
+// }
+//=================================================================================
+
+// 10. Write a program to print a table of 5.
+
+// #include<stdio.h>
+// int main() {
+// int i;
+// for(i=1;i<=10;i++){
+//     printf("5 x %d= %d \n",i,5*i);
+// }
+// return 0;
+// }
