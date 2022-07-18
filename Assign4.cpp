858cc9054d2129fddfe967441368ff41019dfa87
Codebulit <109361363+Codebulit@users.noreply.github.com>
2022-07-19 01:05:43 +0530

    Update Assign4.cpp

--- Assign4.cpp
+++ Assign4.cpp
@@ -111,7 +111,7 @@
 // #include<stdio.h>
 // int main() {
 // int i;
-// printf("squares of the first 10 natural numbers: \n");
+// printf("cube of the first 10 natural numbers: \n");
 // for(i=1;i<=10;i++){
 //      printf("%d ",i*i*i);
 // }
